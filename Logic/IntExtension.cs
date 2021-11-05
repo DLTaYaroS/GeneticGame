4fbed5c2cf502da717963d1e76358d5bf27145ff
DLTaYaroS <62904904+DLTaYaroS@users.noreply.github.com>
2021-11-05 15:47:53 +0100

    Add files via upload

---
+++ Logic/IntExtension.cs
@@ -0,0 +1,12 @@
+﻿using GeneticGameLogic.Manager;
+
+namespace GeneticGameLogic
+{
+    public static class IntExtension
+    {
+        public static void  MutateChange(this int Count, int lvl)
+        {
+            Count += DataManager.GetInstance().rand.Next(-lvl, lvl);
+        }
+    }
+}
